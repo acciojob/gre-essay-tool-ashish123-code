b3b719b23523d592ba82c574187553981f333f7f
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-20 16:35:09 +0530

    Gre essay tool

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let textbox = document.getElementById("evaluatedText");
+let wordCount = document.getElementById("wordCount");
+
+textbox.addEventListener("input", function() {
+  let words = this.value.split(" ");
+  wordCount.textContent = words.length;
+});
