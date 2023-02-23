bb6fac772e33b0c86ba29bb0010b4ce498650391
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-23 20:59:51 +0530

    gh

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //your code here
 
+// Get the evaluatedText and wordCount elements
+const evaluatedText = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+// Define the function to count the words
 function countWords() {
-  var text = document.getElementById("evaluatedText").value;
-  var wordCount = text.trim().split(/\s+/).length;
-  document.getElementById("wordCount").innerHTML = wordCount;
+  const text = evaluatedText.value.trim();
+  const count = text.length > 0 ? text.split(/\s+/).length : 0;
+  wordCount.innerHTML = count;
 }
+
+// Add an event listener to the evaluatedText element
+evaluatedText.addEventListener("input", countWords);
